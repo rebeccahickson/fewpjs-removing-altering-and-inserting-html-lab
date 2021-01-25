71782998a7cb36454aba235be7b8c12d61cf50db
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-25 13:29:18 -0500

    commit tree a0dddba4497b0284b6492d74633b362d008382c5

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let element = document.querySelector("main#main");
+element.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "YOUR-NAME is the champion";
